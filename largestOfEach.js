92e3f447de67264bcfcd4b61c2ec31ef4e78a809
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-22 17:04:32 -0400

    Kyle Thousand

--- largestOfEach.js
+++ largestOfEach.js
@@ -1,7 +1,13 @@
 //LARGEST OF EACH ARRAY
 
-const largestOfEach = arr => {
-
+const largestOfEach = (arr) => {
+  let arr2 = [];
+  arr.map((arr1) => {
+    arr2.push(Math.max(...arr1));
+    console.log(arr2);
+    // return arr2;
+  });
+  return arr2;
 };
 
 console.log(
@@ -9,10 +15,10 @@ console.log(
     [4, 5, 1, 3],
     [13, 27, 18, 26],
     [32, 35, 37, 39],
-    [1000, 1001, 857, 1]
+    [1000, 1001, 857, 1],
   ])
 );
 
 module.exports = {
-  largestOfEach
+  largestOfEach,
 };
